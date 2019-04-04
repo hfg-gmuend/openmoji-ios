ea14a8401804bd6c9b6064fc386d4434bb7c732f
Sam <Samraven@olivereckert.com>
2019-04-04 10:59:27 +0200

    Adds iOS Project

---
+++ OpenMoji Stickers/OpenMoji/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenMoji
+//
+//  Created by Sam Eckert on 04.04.19.
+//  Copyright © 2019 Sam0711er. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
