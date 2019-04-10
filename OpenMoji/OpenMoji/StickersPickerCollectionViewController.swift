e3e4ab6387275a3a83ecc41edc18d5d976f66e04
Sam <Samraven@olivereckert.com>
2019-04-10 22:04:54 +0200

    Inset & Resign fixes

--- OpenMoji/OpenMoji/StickersPickerCollectionViewController.swift
+++ OpenMoji/OpenMoji/StickersPickerCollectionViewController.swift
@@ -14,9 +14,9 @@ class StickersPickerCollectionViewController: UICollectionViewController, UIText
     // MARK: - Properties
     private let reuseIdentifier = "StickerCell"
     private let itemsPerRow: CGFloat = 4
-    private let sectionInsets = UIEdgeInsets(top: 50.0,
+    private let sectionInsets = UIEdgeInsets(top: 20.0,
                                              left: 20.0,
-                                             bottom: 50.0,
+                                             bottom: 20.0,
                                              right: 20.0)
     
     
@@ -160,7 +160,9 @@ class StickersPickerCollectionViewController: UICollectionViewController, UIText
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
-        
+        if filterText.isEmpty || filterText == "" || filterText == " "{
+            cancelSearch()
+        }
         return true
     }
     
