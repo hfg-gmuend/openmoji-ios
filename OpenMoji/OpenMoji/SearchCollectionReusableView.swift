e64530428637b8745c96a988ca64f3fe32a6d0ce
Sam <Samraven@olivereckert.com>
2019-04-10 21:20:24 +0200

    Adds Search

---
+++ OpenMoji/OpenMoji/SearchCollectionReusableView.swift
@@ -0,0 +1,14 @@
+//
+//  SearchCollectionReusableView.swift
+//  OpenMoji
+//
+//  Created by Sam Eckert on 10.04.19.
+//  Copyright © 2019 OpenMoji. All rights reserved.
+//
+
+import UIKit
+
+class SearchCollectionReusableView: UICollectionReusableView {
+        
+    @IBOutlet var searchBar: UISearchBar!
+}
--- OpenMoji/OpenMoji/StickersPickerCollectionViewController.swift
+++ OpenMoji/OpenMoji/StickersPickerCollectionViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Unbox
 
-class StickersPickerCollectionViewController: UICollectionViewController {
+class StickersPickerCollectionViewController: UICollectionViewController, UITextFieldDelegate {
 
     // MARK: - Properties
     private let reuseIdentifier = "StickerCell"
@@ -21,12 +21,16 @@ class StickersPickerCollectionViewController: UICollectionViewController {
     
     
     var stickersArray = [Sticker]()
+    var isFiltering = false
+    var filterText = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.title = "OpenMoji"
         
         getDataFromJSON { (successfullyParsed) in
+            
             if successfullyParsed{
                 self.collectionView.reloadData()
                 self.collectionView.isHidden = false
@@ -34,22 +38,11 @@ class StickersPickerCollectionViewController: UICollectionViewController {
                 self.collectionView.isHidden = true
             }
         }
-        
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-//        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
     }
     
     // MARK: - Data
     func getDataFromJSON(completion: (Bool) -> Void){
         if let jsonFilePath = Bundle.main.url(forResource: "openmoji", withExtension: "json"){
-            //if let jsonFile = FileManager.reso
-            
             do{
                 let jsonFileData = try Data(contentsOf: jsonFilePath)
                 let stickers: [Sticker] = try unbox(data: jsonFileData)
@@ -66,17 +59,74 @@ class StickersPickerCollectionViewController: UICollectionViewController {
         }
     }
     
-    
+    // MARK: - UI
+    var searchTextField = UITextField()
+    @IBOutlet var searchButton: UIBarButtonItem!
+    @IBAction func searchButtonAction(_ sender: Any) {
+        searchTextField.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
+        searchTextField.backgroundColor = .white
+        searchTextField.delegate = self
+        searchTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+        self.navigationItem.setRightBarButton(UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(self.cancelSearch)), animated: true)
+        self.navigationItem.titleView = self.searchTextField
+        
+        
+        searchTextField.becomeFirstResponder()
     }
-    */
+    
+    @objc func cancelSearch(){
+        self.navigationItem.setRightBarButton(self.searchButton, animated: true)
+        self.searchTextField.text = nil
+        self.navigationItem.titleView = nil
+
+        self.title = "OpenMoji"
+       
+        searchTextField.removeTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        isFiltering = false
+        self.collectionView?.reloadData()
+    }
+    
+    //MARK: - SEARCH
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        if textField.text == nil || textField.text?.isEmpty ?? true || textField.text == "" || textField.text == " "{
+            print("isFiltering will be set to false")
+            isFiltering = false
+            self.collectionView?.reloadData()
+        }else{
+            updateSearchResults(textField.text!) {
+                self.collectionView?.reloadData()
+            }
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("textFieldDidEndEditing")
+        if filterText == nil || filterText.isEmpty || filterText == "" || filterText == " "{
+            cancelSearch()
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    func updateSearchResults(_ searchText: String, completion: () -> Void){
+        isFiltering = true
+        filterText = searchText
+        completion()
+    }
+    
+    func filteredArray() -> [Sticker]{
+        if filterText != ""{
+            return stickersArray.filter{ $0.annotation!.contains(filterText) || $0.hexcode!.contains(filterText) || $0.emoji!.contains(filterText) || $0.group!.contains(filterText) || $0.subgroups!.contains(filterText) || $0.tags!.contains(filterText) }
+        }else{
+            return [Sticker]()
+        }
+    }
+
 
     // MARK: UICollectionViewDataSource
 
@@ -88,60 +138,105 @@ class StickersPickerCollectionViewController: UICollectionViewController {
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return stickersArray.count
+        if isFiltering{
+            //print("filteredArray is \(filteredArray())")
+            return filteredArray().count
+        }else{
+            return stickersArray.count
+        }
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! StickersPickerCollectionViewCell
     
-        if let hexcode = stickersArray[indexPath.row].hexcode{
-            let imageName = "stickers/\(hexcode)"
-            print(imageName)
-            
-            // Configure the cell
-            if let stickerImage = UIImage(named: imageName){
-                cell.imageView.image = stickerImage
+        if isFiltering{
+            if let hexcode = filteredArray()[indexPath.row].hexcode{
+                let imageName = "stickers/\(hexcode)"
+                //print(imageName)
+                
+                // Configure the cell
+                if let stickerImage = UIImage(named: imageName){
+                    cell.imageView.image = stickerImage
+                    cell.imageView.isHidden = false
+                    cell.backgroundColor = UIColor.clear
+                }else{
+                    cell.imageView.image = nil
+                    cell.imageView.isHidden = true
+                    cell.backgroundColor = UIColor.darkGray
+                }
+            }else{
+                cell.imageView.image = nil
+                cell.imageView.isHidden = true
+                cell.backgroundColor = UIColor.darkGray
             }
-            
         }else{
-            cell.imageView.image = nil
+            if let hexcode = stickersArray[indexPath.row].hexcode{
+                let imageName = "stickers/\(hexcode)"
+                //print(imageName)
+                
+                // Configure the cell
+                if let stickerImage = UIImage(named: imageName){
+                    cell.imageView.image = stickerImage
+                    cell.imageView.isHidden = false
+                    cell.backgroundColor = UIColor.clear
+                }else{
+                    cell.imageView.image = nil
+                    cell.imageView.isHidden = true
+                    cell.backgroundColor = UIColor.darkGray
+                }
+            }else{
+                cell.imageView.image = nil
+                cell.imageView.isHidden = true
+                cell.backgroundColor = UIColor.darkGray
+            }
         }
         
         return cell
 
     }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
     
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        if isFiltering{
+            if let hexcode = filteredArray()[indexPath.row].hexcode{
+                let imageName = "stickers/\(hexcode)"
+                //print(imageName)
+                
+                // Configure the cell
+                if let stickerImage = UIImage(named: imageName){
+                    UIPasteboard.general.image = stickerImage
+                    print("Copied image")
+                }
+            }
+        }else{
+            if let hexcode = stickersArray[indexPath.row].hexcode{
+                let imageName = "stickers/\(hexcode)"
+                //print(imageName)
+                
+                // Configure the cell
+                if let stickerImage = UIImage(named: imageName){
+                    UIPasteboard.general.image = stickerImage
+                    print("Copied image")
+                }
+            }
+        }
+        
     }
-    */
-
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        if (kind == UICollectionView.elementKindSectionHeader) {
+            let headerView:UICollectionReusableView =  collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "CollectionViewSearchHeader", for: indexPath)
+            
+            return headerView
+        }
+        
+        return UICollectionReusableView()
+        
+    }
+    
+    
+   
 }
 
 extension StickersPickerCollectionViewController: UICollectionViewDelegateFlowLayout {
